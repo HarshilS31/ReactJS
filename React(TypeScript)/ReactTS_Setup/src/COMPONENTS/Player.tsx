3f4f447c74b0bd75af4353c91ee278ea15a7b288
Harshil Sharma <harshils3107@gmail.com>
2026-06-11 16:35:58 +0530

    New files added

--- React(TypeScript)/ReactTS_Setup/src/COMPONENTS/Player.tsx
+++ React(TypeScript)/ReactTS_Setup/src/COMPONENTS/Player.tsx
@@ -2,8 +2,9 @@ import type { ReactNode } from "react"
 
 // type PlayerDetails={name:string,club:string}
 interface i1 {
-  children:ReactNode
+  children:ReactNode //use ReactNode whenever we import a JSX component! 
 }
+// const Player :FC<i1> ({chiildren}) =>{ ODL WAY OF IMPORTING!
 const Player = ({children}:i1) => {
   return (
     <div>
